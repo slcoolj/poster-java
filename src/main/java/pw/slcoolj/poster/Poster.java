6005b09642cd3e819fed725866f7086a7300a2aa
Maxwell Cody <maxwell@cody.sh>
2019-10-01 18:18:33 -0500

    improved

--- src/main/java/pw/slcoolj/poster/Poster.java
+++ src/main/java/pw/slcoolj/poster/Poster.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,27 +15,30 @@ import java.util.List;
  * @author sl
  */
 public class Poster {
-    private final String url, parameters, proxyHost, proxyPort;
-    private List<String> replyList = new ArrayList<>();
-    private final boolean proxy;
+    private final URL url;
+    private final String parameters, proxyHost, proxyPort;
+    private List<String> replyList;
 
-    public Poster(String url, String parameters) {
+    public Poster(URL url, String parameters, String proxyHost, String proxyPort) {
         this.url = url;
         this.parameters = parameters;
-        this.proxy = false;
-        this.proxyHost = "";
-        this.proxyPort = "";
-    }
-
-    public Poster(String url, String parameters, String proxyHost, String proxyPort) {
-        this.url = url;
-        this.parameters = parameters;
-        this.proxy = true;
         this.proxyHost = proxyHost;
         this.proxyPort = proxyPort;
     }
 
-    public String getURL() {
+    public Poster(URL url, String parameters) {
+        this(url, parameters, null, null);
+    }
+
+    public Poster(String url, String parameters) throws MalformedURLException {
+        this(new URL(url), parameters, null, null);
+    }
+
+    public Poster(String url, String parameters, String proxyHost, String proxyPort) throws MalformedURLException {
+        this(new URL(url), parameters, proxyHost, proxyPort);
+    }
+
+    public URL getURL() {
         return url;
     }
 
@@ -43,7 +47,7 @@ public class Poster {
     }
 
     public boolean isUsingProxy() {
-        return proxy;
+        return proxyHost != null || proxyPort != null;
     }
 
     public String getProxyHost() {
@@ -59,12 +63,18 @@ public class Poster {
     }
 
     public void sendPost() {
-        if (proxy) {
+        if(replyList != null)
+            throw new IllegalStateException("Post has already been sent.");
+
+        if (proxyHost != null) {
             System.setProperty("http.proxyHost", proxyHost);
+        }
+
+        if (proxyPort != null) {
             System.setProperty("http.proxyPort", proxyPort);
         }
+
         try {
-            URL url = new URL(this.url);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setDoOutput(true);
             connection.setDoInput(true);
@@ -78,11 +88,11 @@ public class Poster {
 
             DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
             outputStream.writeBytes(parameters);
-            outputStream.flush();
             outputStream.close();
             connection.disconnect();
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line;
+            replyList = new ArrayList<>();
             while((line = reader.readLine()) != null) {
                 replyList.add(line);
             }
